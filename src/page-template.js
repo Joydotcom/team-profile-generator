078c5bf6ff454c20a5e61eb7da8f1bc6badcf278
joydotcom <msjoyjackson@gmail.com>
2021-04-13 19:59:43 -0400

    added methods on the page template for manager, intern, and engineer.

--- src/page-template.js
+++ src/page-template.js
@@ -1,13 +1,56 @@
 // generate the HTML pages
 const generateTeam = team => {
-     // A method for a template to render manager info
-
-    // A method for a template to render engineer info
-
-    // A method for a template to render intern info
+  
 const  generateManager = (manager) => {
-    return ``;
+    return `<div class="card employee-card">
+    <div class="card-header">
+        <h2 class="card-title">${manager.getName()}</h2>
+        <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">ID: ${manager.getId()}</li>
+            <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+            <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
+        </ul>
+    </div>
+</div>`;
 }
+
+const  generateEngineer = (engineer) => {
+    return `<div class="card employee-card">
+    <div class="card-header">
+        <h2 class="card-title">${engineer.getName()}</h2>
+        <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${engineer.getRole()}</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">ID: ${engineer.getId()}</li>
+            <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+            <li class="list-group-item">Office number: ${engineer.getOfficeNumber()}</li>
+        </ul>
+    </div>
+</div>`;
+}
+
+const  generateIntern = (intern) => {
+    return `<div class="card employee-card">
+    <div class="card-header">
+        <h2 class="card-title">${intern.getName()}</h2>
+        <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${intern.getRole()}</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">ID: ${intern.getId()}</li>
+            <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+            <li class="list-group-item">Office number: ${intern.getOfficeNumber()}</li>
+        </ul>
+    </div>
+</div>`;
+}
+
+
+
 
 const html = [];
     html.push(team
@@ -17,6 +60,10 @@ const html = [];
     html.push(team
         .filter(employee => employee.getRole() === "Engineer")
         .map(engineer => generateEngineer(engineer))
+    );
+    html.push(team
+        .filter(employee => employee.getRole() === "Intern")
+        .map(engineer => generateEngineer(intern))
     );
     return html.join("");
 }
